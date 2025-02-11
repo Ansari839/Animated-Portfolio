933c99f3e60415e44072e37b1e56a3fcd888449f
Abdullah Ansari <137299911+Ansari839@users.noreply.github.com>
2025-02-11 08:01:01 +0000

    New Upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // Ensures Next.js generates static files for GitHub Pages
   images: {
-    unoptimized: true, // Fixes image optimization issue on GitHub Pages
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+    ],
   },
-  basePath: "/your-repo-name", // Replace with your actual GitHub repo name
 };
 
 export default nextConfig;
